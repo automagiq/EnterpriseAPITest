2fd53c0e99b4a1be81782be38216f4efbc529133
Zafar Tagiyev <geko.tm@gmail.com>
2023-10-12 00:01:53 -0600

    Your update message

---
+++ Models/ADTResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace EnterpriseAPITest.Models
+{
+    public class ADTResponse
+    {
+        public string Message { get; set; }
+        public ADP Data { get; set; }
+    }
+
+    public class ADP
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Tests/ADTTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace EnterpriseAPITest.Tests
+{
+    public class ADTConfigurationTests
+    {
+        private HttpClient _client;
+
+        [SetUp]
+        public void Setup()
+        {
+            _client = new HttpClient();
+        }
+
+        [Test]
+        public async Task ValidateADTIsConfigured()
+        {
+            // Setup data for token request
+            var tokenUrl = "https://reidentityws.devint.dev-r5ead.net/connect/token";
+            var requestData = new Dictionary<string, string>
+            {
+                { "grant_type", "client_credentials" },
+                { "client_id", "appsdataclient" },
+                { "client_secret", "secret" }
+            };
+
+            // Request token
+            var response = await _client.PostAsync(tokenUrl, new FormUrlEncodedContent(requestData));
+            response.EnsureSuccessStatusCode();
+
+            // Extract token from response
+            string token = await response.Content.ReadAsStringAsync(); // directly read the string content
+
+            // Setup request to check if ADT is configured
+            var requestUrl = "https://reappdatamanagerws.devint.dev-r5ead.net/api/IsADTConfigured";
+            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            // Make the request
+            response = await _client.GetAsync(requestUrl);
+
+            // Check status code
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Expected a status code of 200 OK");
+
+            // Check response content
+            bool isADTConfigured = bool.Parse(await response.Content.ReadAsStringAsync());
+            Assert.IsTrue(isADTConfigured, "Expected the response body to be true");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _client.Dispose();
+        }
+    }
+}
